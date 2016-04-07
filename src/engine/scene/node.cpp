c2310ca27f87d5b1ab1fa6d5691556e043b09ab3
David Peicho <peicho_d@epita.fr>
2016-04-07 02:13:48 +0200

    Add basic Node class containing primitive functions

--- src/engine/scene/node.cpp
+++ src/engine/scene/node.cpp
@@ -1,1 +1,31 @@
 #include "node.hpp"
+
+namespace scene
+{
+  Node::Node()
+  { }
+
+  Node::Node(std::string& name)
+  {
+    name_ = name;
+  }
+
+  void
+  Node::add_child(Node::NodePtr n)
+  {
+    children_.push_back(n);
+  }
+
+  void
+  Node::clear_children()
+  {
+    children_.clear();
+  }
+
+  Node::NodePtr&
+  Node::get_child_at(size_t i)
+  {
+    children_.at(i);
+  }
+
+} // namespace scene
---
+++ src/engine/scene/node.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace scene
+{
+  class Node
+  {
+    public:
+      typedef std::shared_ptr<Node>               NodePtr;
+      typedef std::vector<std::shared_ptr<Node>>  NodeList;
+
+    public:
+      Node();
+      Node(std::string& name);
+
+    public:
+      void add_child(NodePtr n);
+      NodePtr& get_child_at(size_t i);
+      void clear_children();
+
+    private:
+      std::string   name_;
+      NodeList      children_;
+
+  };
+
+} // namespace scene
