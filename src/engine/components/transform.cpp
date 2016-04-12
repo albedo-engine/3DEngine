79940ab058eb96a48a1fa7b78cfa7126ae133b31
David Peicho <peicho_d@epita.fr>
2016-04-12 10:35:02 +0200

    Update Transform adding the rotate and scale functions

--- src/engine/components/transform.cpp
+++ src/engine/components/transform.cpp
@@ -9,20 +9,18 @@ namespace Engine
 
     Transform::Transform(glm::vec3 position)
     {
-      world_matrix_ = glm::translate(world_matrix_, position);
+      this->translate(position);
     }
 
     Transform::Transform(glm::vec3 scale, glm::vec3 rot_axis, float angle)
     {
-      world_matrix_ = glm::scale(world_matrix_, scale);
-      world_matrix_ = glm::rotate(world_matrix_, glm::radians(angle), rot_axis);
+      world_matrix_ = glm::rotate(world_matrix_, glm::radians(angle), rot_axis)
+                      * glm::scale(world_matrix_, scale);
     }
 
     Transform::Transform(glm::vec3 scale, glm::vec3 position, glm::vec3 rot_axis, float angle)
     {
-      world_matrix_ = glm::scale(world_matrix_, scale);
-      world_matrix_ = glm::rotate(world_matrix_, glm::radians(angle), rot_axis);
-      world_matrix_ = glm::translate(world_matrix_, position);
+      this->scale_rot_translate(scale, position, rot_axis, angle);
     }
 
     void
@@ -33,23 +31,42 @@ namespace Engine
 
     void
     Transform::translate(double x, double y, double z)
-    { }
+    {
+      this->translate(glm::vec3(x, y, z));
+    }
 
     void
-    Transform::rotate(double angle, glm::vec3 axis)
-    { }
+    Transform::rotate(float angle, glm::vec3 rot_axis)
+    {
+      world_matrix_ = glm::rotate(world_matrix_, glm::radians(angle), rot_axis);
+    }
 
     void
-    Transform::rotate(double angle, double xaxis, double yaxis, double zaxis)
-    { }
+    Transform::rotate(float angle, double xaxis, double yaxis, double zaxis)
+    {
+      this->rotate(angle, glm::vec3(xaxis, yaxis, zaxis));
+    }
 
     void
     Transform::scale(glm::vec3 factor)
-    { }
+    {
+      world_matrix_ = glm::scale(world_matrix_, factor);
+    }
 
     void
     Transform::scale(double xaxis, double yaxis, double zaxis)
-    { }
+    {
+      this->translate(glm::vec3(xaxis, yaxis, zaxis));
+    }
+
+    void
+    Transform::scale_rot_translate(glm::vec3 scale, glm::vec3 position,
+                                   glm::vec3 rot_axis, float angle)
+    {
+      world_matrix_ = glm::translate(world_matrix_, position)
+                      * glm::rotate(world_matrix_, glm::radians(angle), rot_axis)
+                      * glm::scale(world_matrix_, scale);
+    }
 
   } // namespace Component
 } // namespace Engine
--- src/engine/components/transform.hpp
+++ src/engine/components/transform.hpp
@@ -21,11 +21,14 @@ namespace Engine
         void translate(glm::vec3 position);
         void translate(double x, double y, double z);
 
-        void rotate(double angle, glm::vec3 axis);
-        void rotate(double angle, double xaxis, double yaxis, double zaxis);
+        void rotate(float angle, glm::vec3 axis);
+        void rotate(float angle, double xaxis, double yaxis, double zaxis);
 
         void scale(glm::vec3 scale_vector);
         void scale(double scale_x, double scale_y, double scale_z);
+
+        void scale_rot_translate(glm::vec3 scale, glm::vec3 position,
+                                 glm::vec3 rotation, float angle);
 
       private:
         glm::mat4 world_matrix_;
