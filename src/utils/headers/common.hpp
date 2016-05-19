ad7d6b5007db70200dda39ffb7c512b738e31578
David Peicho <peicho_d@epita.fr>
2016-05-20 01:02:17 +0200

    Add the glew.h header to the common.hpp includes file

--- src/utils/headers/common.hpp
+++ src/utils/headers/common.hpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <algorithm>
 
+#include <GL/gl.h>
+
 #include "../glm/glm/glm.hpp"
 #include "../glm/glm/gtc/matrix_transform.hpp"
 #include "../glm/glm/gtc/type_ptr.hpp"
