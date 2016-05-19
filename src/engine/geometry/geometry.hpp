dbf67d27530e42382bd52710563222aca43dbbf8
David Peicho <peicho_d@epita.fr>
2016-05-19 23:52:46 +0200

    Add the indices vector to the Geometry class

--- src/engine/geometry/geometry.hpp
+++ src/engine/geometry/geometry.hpp
@@ -12,15 +12,22 @@ namespace Engine
       glm::vec3 position;
       glm::vec3 normal;
       glm::vec2 text_coords;
-    }
+    };
 
     class Geometry : public std::enable_shared_from_this<Geometry>
     {
+      public:
+        typedef std::shared_ptr<Geometry>  GeometryPtr;
+
       public:
         Geometry();
 
       private:
         std::vector<Vertex> vertices_;
+        std::vector<GLuint> indices_;
+
+        GLuint vao_;
+        GLuint vbo_;
     };
   } // namespace Geometry
 } // namespace Geometry
