64daa3b6b76c05126bfdc28eb8e93801c4d8d8a7
David Peicho <peicho_d@epita.fr>
2016-05-30 00:52:06 +0200

    Add basic test suite for the Transform component

--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -4,6 +4,8 @@
 
 #include <utils/headers/common.hpp>
 #include <scene/node.hpp>
+#include <components/transform/transform.hpp>
+#include <components/geometry/geometry.hpp>
 #include <components/camera/perspective-camera.hpp>
 
 namespace Engine
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,7 +6,43 @@ using namespace Engine;
 using namespace Engine::Scene;
 using namespace Engine::Components;
 
+void test_transform()
+{
+  Node::NodePtr parent = Node::create("parent");
+  parent->add_component(Transform::create());
+
+  Node::NodePtr child = Node::create();
+
+  ASSERT(parent->component<Transform>() != nullptr, "get existing component");
+  ASSERT(parent->component<Geometry>() == nullptr, "get non-existing component");
+  ASSERT(parent->component<Transform>()->get_local_position() == glm::vec3(0, 0, 0), "check local position");
+  ASSERT(parent->get_parent() == nullptr, "check for null parents");
+  ASSERT(parent->component<Transform>()->get_world_position() == glm::vec3(0, 0, 0), "check world position");
+
+  // Checks position without parent
+  parent->component<Transform>()->translate(glm::vec3(5, 0, 0));
+  ASSERT(parent->component<Transform>()->get_local_position() == glm::vec3(5, 0, 0), "check local position");
+  ASSERT(parent->component<Transform>()->get_world_position() == glm::vec3(5, 0, 0), "check world position");
+
+  // Checks position before adding parent
+  child->add_component(Transform::create());
+  child->component<Transform>()->translate(glm::vec3(0, 1, 0));
+  ASSERT(child->component<Transform>()->get_local_position() == glm::vec3(0, 1, 0), "check local position");
+  ASSERT(child->component<Transform>()->get_world_position() == glm::vec3(0, 1, 0), "check world position");
+
+  // Checks position relative to parent
+  parent->add_child(child);
+  ASSERT(child->component<Transform>()->get_local_position() == glm::vec3(0, 1, 0), "check local position");
+  ASSERT(child->component<Transform>()->get_world_position() == glm::vec3(5, 1, 0), "check world position");
+}
+
 void test_components()
+{
+  // Executes the test suite on the Transform class
+  test_transform();
+}
+
+void test_node()
 {
   Node::NodePtr parent = Node::create("parent");
   Node::NodePtr child = Node::create();
@@ -32,6 +68,7 @@ void test_camera()
 int main()
 {
   // Tests for components
+  LAUNCH(test_node);
   LAUNCH(test_components);
   LAUNCH(test_camera);
 
